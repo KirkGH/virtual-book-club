15633a522c4b52b0b8727ae1f1e0c7600f3dc3d2
Heather Yuen <hkhkyuen28@gmail.com>
2024-12-08 12:20:00 -0500

    styling for calendar pages added

--- app/public/calendar/script.js
+++ app/public/calendar/script.js
@@ -1,43 +1,43 @@
 let addButton = document.getElementById('addsubmit');
 let addSuccessMessage = document.getElementById('addmessage');
 addButton.addEventListener("click", async (event) => {
-  // event.preventDefault();
+    // event.preventDefault();
 
-  let userClub = document.getElementById('clubname').value;
-  let userTitle = document.getElementById('addtitle').value;
-  let userStart = document.getElementById('addstart').value;
-  //console.log(document.getElementById('addstart'), 
-  //  document.getElementById('addstart').value)
-  let userEnd = document.getElementById('addend').value;
+    let userClub = document.getElementById('clubname').value;
+    let userTitle = document.getElementById('addtitle').value;
+    let userStart = document.getElementById('addstart').value;
+    //console.log(document.getElementById('addstart'), 
+    //  document.getElementById('addstart').value)
+    let userEnd = document.getElementById('addend').value;
 
-  let dataToSend = { club: userClub, title: userTitle, startTime: userStart, endTime: userEnd };
-  console.log(dataToSend, JSON.stringify(dataToSend))
+    let dataToSend = { club: userClub, title: userTitle, startTime: userStart, endTime: userEnd };
+    console.log(dataToSend, JSON.stringify(dataToSend))
 
-  try {
-      let response = await fetch('/events', {
-          method: 'POST',
-          headers: {'Content-Type': 'application/json'},
-          body: JSON.stringify(dataToSend)
-      });
+    try {
+        let response = await fetch('/events', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(dataToSend)
+        });
 
-      if (response.status === 200 || response.status === 201) {
-        addSuccessMessage.textContent = 'Success';
-      } else {
-        addSuccessMessage.textContent = 'Bad request';
-      }
+        if (response.status === 200 || response.status === 201) {
+            addSuccessMessage.textContent = 'Success';
+        } else {
+            addSuccessMessage.textContent = 'Bad request';
+        }
 
-  } catch (error) {
-      console.error('Error:', error);
-  }
+    } catch (error) {
+        console.error('Error:', error);
+    }
 });
 
 async function loadEventAndClubData() {
     try {
         const response = await fetch('/events'); // No need for full URL if hosted on the same domain/port
         if (!response.ok) throw new Error('Failed to fetch event and club data');
-        
+
         const data = await response.json();
-        
+
         const clubs = data.clubs;
         const clubSelect = document.getElementById('clubname');
 
